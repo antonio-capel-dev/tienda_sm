508c8556022f0f9d2d8847ec8a6c5aca7fbc3d39
Antonio Miguel Capel Ramírez <antoniomiguelcapelramirez@gmail.com>
2025-10-28 11:40:52 +0100

    Entrega con la clase impuesto

--- classes/impuestos.php
+++ classes/impuestos.php
@@ -1,9 +1,49 @@
 <?php
+    class Impuestos {
+    public $id;
+    public $nombre
+    public $porcentaje;0
+    public $porcentaje;
+    public $activo;
+    private $db;
+        /**Constructor al crear la clase impuestos */
+    public function __constructor(db) {
+        $this->db =$db;
+    }
 
+    public function nuevo() {
+        $sql = "INSERT INTO impuestos (nombre, porcentaje, activo)
+                VALUES (:nombre, :porcentaje, :activo)";
 
+        $stmt = $this->db->prepare($sql);
 
+        $stmt->execute([
+            'nombre' => $this>nombre,
+            'porcentaje' => $this->activo
+        ]);
 
+        $this->id = $this->db->lastInsertID();
 
+        return $this->id;
+    }
+
+    public function obtenerPorId($id) {
+        $sql = "SELECT * FROM impuestos WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['id' => $id]);
+
+        //Obtener datos
+        $datos = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($datos) {
+            $this->id = $datos['id'];
+            $this->nombre = $datos['nombre'];
+            $this->porcentaje = $datos['porcentaje'];
+            $this->activo = $datos['activo'];
+        }
+        return $datos;
+    }
+    }
 
 
 
--- classes/impuestosclass.php
+++ classes/impuestosclass.php
@@ -11,22 +11,63 @@
     public function __construct(PDO $db) {
         $this->db = $db;
     }
+    /**
+     * METODO: listado() - Obtiene TODOS los impuestos. This method return an array with all the register.
+     */
+
+    public function listado() {
+        try {
+            $sql = 'SELECT id, nombre, porcentaje, activo' .
+            'FROM impuestos ' .
+            'WHERE activo = 1 ' .
+            'ORDER BY id DESC';
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            error_log("Error en el listado: " . $e->getMessage());
+            return [];
+        }
+    }
 
     public function listarDB($pagina, $paginado){
-            $stmt = $this->db->prepare( 'SELECT id_producto, nombre, descripcion, precio FROM producto LIMIT .$inicio., .$paginado.');
-            $stmt->execute();
+        try {
+            $pagina -= 1;
+            $inicio = $pagina * $paginado;
+
+            $sql = 'SELECT id, nombre, porcentaje, activo ' .
+            'FROM impuestos ' . 
+            'ORDER BY id DESC LIMIT ?, ?';
+        
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute($inicio, $paginado);
+
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }   catch (PDOException $e) {
+            error_log("Error: " . $e->getMessage());
+            return [];
+        }
             $total = $stmt->fetch(PDO::FETCH_ASSOC)["total"];
-
             $pagina-=1;
-
             $arrADevolver=array();
-            
             $inicio=$pagina * $paginado;
-
             $stmt = $this->db->prepare('
-                                    SELECT id_producto, nombre, descripcion, precio FROM producto LIMIT .$inicio., .$paginado.');
-
+                    SELECT id_impuesto, nombre, descripcion, precio FROM impuestos LIMIT .$inicio., .$paginado.');
             $fin=$inicio + $paginado;
 }
+
+    public function listar($pagina, $paginado) {
+            $arrayAdevolver=array();
+            $pagina -=1;
+            $listado = $this->listado();
+
+            $inicio=$pagina * $paginado;
+            $fin=$inicio + $paginado;
+            if($fin>count) {
+
+            }
+    }
     }
 ?>
